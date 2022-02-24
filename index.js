b3794dff944aca1b898785ca45e0ab12598449fd
JonissonGomes <joni_gomes@hotmail.com>
2022-02-24 20:38:32 -0300

    [Finalizando Projeto Parte I]
    Co-authored-by: verasmarianna <verasmarianna@users.noreply.github.com>
    Co-authored-by: Raquel Vargas <vargasraquel@users.noreply.github.com>
    Co-authored-by: Allan Garcia <allanfsgarcia@users.noreply.github.com>
    Co-authored-by: Gizele Rodrigues <gizele233@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,50 @@
+// Importação da biblioteca
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require('fs');
+
+async function run() {
+  // Criar uma variável para armazenar o link da globo
+  const url = "https://g1.globo.com/";
+  
+// Seletor Main
+const MAINSELETOR = '.feed-post-body'  
+
+// seletor para buscar a imagem
+const IMAGEM = ".bstn-fd-picture-image";
+
+// seletor para buscar o título
+const TITULO = ".feed-post-link";
+
+// seletor para buscar os subtítulos
+const SUBTITULO = ".feed-post-header-chapeu";
+
+// seletor para buscar o link para a matéria
+const LINK = ".feed-post-link";
+
+  const dados = await axios.get(url).then((res) => {
+        let $ = cheerio.load(res.data);
+        let body = [];
+
+        // Pegando Imagem e inserindo no JSON
+        $(MAINSELETOR).each((index, elem) => {
+            body.push({
+                image: $(elem).find(IMAGEM).attr('src'),
+                title: $(elem).find(TITULO).text(),
+                subtitle: $(elem).find(SUBTITULO).text(),
+                link: $(elem).find(LINK).attr('href'),
+            }) 
+        })
+
+        fs.writeFile('./body.json', JSON.stringify(body), (error) => {
+            if (error) throw error;
+        })
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+}
+
+run();
